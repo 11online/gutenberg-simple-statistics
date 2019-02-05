138bdb3365ecf2ad17f6f3f54a4362507d057c37
Eric Debelak <ericdebelak@gmail.com>
2019-02-04 17:48:12 -0700

    FIXED: import

--- src/init.php
+++ src/init.php
@@ -66,7 +66,7 @@ function gutenberg_simple_statistics_cgb_editor_assets() {
 	wp_enqueue_script(
 		'gutenberg_simple_statistics-cgb-block-js', // Handle.
 		plugins_url( '/dist/blocks.build.js', dirname( __FILE__ ) ), // Block.build.js: We register the block here. Built with Webpack.
-		array( 'wp-blocks', 'wp-i18n', 'wp-element' ) // Dependencies, defined above.
+		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor' ) // Dependencies, defined above.
 		// filemtime( plugin_dir_path( __FILE__ ) . 'block.js' ) // Version: filemtime — Gets file modification time.
 	);
 
